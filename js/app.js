5bd0118074d39fdb995dd7323f26281aa63b64d0
Nicolas Caruso <nico.caruso1992@outlook.com>
2019-07-05 21:39:21 -0300

    Se arregla el bug que cuando se apretaba en las letras no surtia efecto

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ window.addEventListener("keydown", pressKey);
 window.addEventListener("click", click);
 
 function click(e) {
-    const data = e.target.attributes[1].nodeValue;
+    const data = e.target.attributes[0].nodeValue;
     const audio = document.querySelector(`audio[data-key="${data}"]`);
     const key = document.querySelector(`.key[data-key="${data}"]`);
+    const kbd = document.querySelector(`kbd[data-key="${data}"]`);
+    const span = document.querySelector(`span[data-key="${data}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-    if(e.target == key) {
+    if(e.target == key || e.target == kbd || e.target == span) {
         key.classList.add("playing");
         key.addEventListener("transitionend", function() {
             key.classList.remove("playing");
